0c9d446e44178e1711831b66ff6001b83e1bfe5f
wliu <wliu@radiant.net>
2020-07-23 21:34:55 -0700

    Update and Fixed Payment Bug

--- Database/WebPortalMainDB/dbo/Tables/Class_UserPayment.sql
+++ Database/WebPortalMainDB/dbo/Tables/Class_UserPayment.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[Class_UserPayment] (
+CREATE TABLE [dbo].[Class_UserPayment] (
     [Id]           INT           IDENTITY (1, 1) NOT NULL,
     [ClassId]      INT           NULL,
     [UserId]       INT           NULL,
@@ -18,59 +18,62 @@
 );
 
 
-GO
-CREATE TRIGGER TRG_Class_UserPayment_Update
-ON Class_UserPayment
-After Update
-AS
-IF EXISTS( 
-		SELECT Class_Enroll.ClassId, Class_Enroll.UserId 
-				FROM Class_Enroll 
-				INNER JOIN Inserted 
-					On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
-					AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1
-)
-BEGIN
-UPDATE Class_Enroll
- SET 
-	PaidDate = Inserted.PaidDate, 
-	PaidMethod = Inserted.PaidMethod, 
-	PaidInvoice = Inserted.PaidInvoice, 
-	PaidCard = Inserted.PaidCard,
-	IsPaid = 1
-FROM Class_Enroll
-INNER JOIN Inserted 
-					On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
-					AND Inserted.IsSuccess=1
-					AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1 
 
-UPDATE Class_Enroll
- SET 
-	PaidAmount = payment.PaidAmount,
-	IsPaid = 1
-FROM Class_Enroll
-INNER JOIN Inserted 
-					On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
-					AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1
-CROSS APPLY (
-	SELECT Sum(PaidAmount) as PaidAmount
-	FROM Class_UserPayment 
-	WHERE ClassId = Inserted.ClassId AND UserId=Inserted.UserId AND IsSuccess=1
-) payment
+
+GO
+
+GO
+CREATE TRIGGER [dbo].[TRG_Class_UserPayment]
+ON [dbo].[Class_UserPayment]
+After Insert, Update
+AS
+BEGIN
+
+	IF EXISTS( 
+			SELECT Class_Enroll.ClassId, Class_Enroll.UserId 
+					FROM Class_Enroll 
+					INNER JOIN Inserted 
+						On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
+						AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1
+	)
+	BEGIN
+		UPDATE Class_Enroll
+		 SET 
+			PaidDate = Inserted.PaidDate, 
+			PaidMethod = Inserted.PaidMethod, 
+			PaidInvoice = Inserted.PaidInvoice, 
+			PaidCard = Inserted.PaidCard,
+			IsPaid = 1
+		FROM Class_Enroll
+		INNER JOIN Inserted 
+							On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
+							AND Inserted.IsSuccess=1
+							AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1 
+
+		UPDATE Class_Enroll
+		 SET 
+			PaidAmount = payment.PaidAmount,
+			IsPaid = 1
+		FROM Class_Enroll
+		INNER JOIN Inserted 
+							On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
+							AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1
+		CROSS APPLY (
+			SELECT Sum(PaidAmount) as PaidAmount
+			FROM Class_UserPayment 
+			WHERE ClassId = Inserted.ClassId AND UserId=Inserted.UserId AND IsSuccess=1
+		) payment
+	END
+
+	IF NOT EXISTS( 
+			SELECT Class_Enroll.ClassId, Class_Enroll.UserId 
+					FROM Class_Enroll 
+					INNER JOIN Inserted 
+						On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
+						AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1
+	)
+	INSERT Class_Enroll(ClassId, UserId, IsPaid, PaidDate, PaidAmount, PaidMethod, PaidInvoice, PaidCard, Deleted, Active)
+	SELECT ClassId, UserId, IsSuccess, PaidDate, PaidAmount, PaidMethod, PaidInvoice, PaidCard, 0, 1
+	FROM Inserted 
 
 END
-GO
-create TRIGGER TRG_Class_UserPayment_Insert
-ON Class_UserPayment
-After Insert
-AS
-IF NOT EXISTS( 
-		SELECT Class_Enroll.ClassId, Class_Enroll.UserId 
-				FROM Class_Enroll 
-				INNER JOIN Inserted 
-					On Inserted.ClassId = Class_Enroll.ClassId AND Inserted.UserId=Class_Enroll.UserId 
-					AND Class_Enroll.Deleted=0 AND Class_Enroll.Active=1
-)
-INSERT Class_Enroll(ClassId, UserId, IsPaid, PaidDate, PaidAmount, PaidMethod, PaidInvoice, PaidCard, Deleted, Active)
-SELECT ClassId, UserId, IsSuccess, PaidDate, PaidAmount, PaidMethod, PaidInvoice, PaidCard, 0, 1
-FROM Inserted
